a80bb9cf79c17dff244b08531358b901be0fa8a5
Frank Schmid <fschmid@stashimi.com>
2017-05-08 13:56:11 +0200

    Added a hook for invoke local. Set environment on service for local invocation. Fixed race condition with run()

---
+++ src/lib/setEnvVariables.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const _ = require("lodash");
+
+/**
+ * Set Serverless environment variables for local invocations
+ *
+ * @param {Serverless} serverless - Serverless Instance
+ * @param {Map<String>} environment - Resolved environment
+ */
+function setEnvVariables(serverless, environment) {
+	const functions = _.get(serverless, "service.functions", {});
+
+	// Set the function environment for all functions that define one
+	_.each(_.keys(functions), funcName => {
+		const funcEnv = functions[funcName].environment;
+
+		if (funcEnv) {
+			_.assign(funcEnv, _.reduce(funcEnv, (acc, value, key) => {
+				acc[key] = environment[key];
+			}, {}));
+		}
+	});
+
+	// Set the global environment
+	_.assign(serverless.service.provider.environment, environment);
+}
+
+module.exports = setEnvVariables;
