62764409dec4ee18d75dd881ea19d3b3f9f6090c
Anders Quist <anders@qinfo.se>
2020-09-17 14:09:42 +0200

    feat: set resource values from stack output

--- src/lib/collectResourcesOutputs.js
+++ src/lib/collectResourcesOutputs.js
@@ -6,14 +6,19 @@ const _ = require("lodash");
  * Collects Serverless Outputs resources
  *
  * @param {Serverless} serverless - Serverless Instance
+ * @param {Array[Map]} stackOutputs - an array of stack outputs
  * @returns {String[]} Returns a list of environment variables
  */
-function collectResourcesOutputs(serverless) {
+function collectResourcesOutputs(serverless, stackOutputs = {}) {
   const outputs = _.get(serverless, "service.resources.Outputs", []);
   const envVars = {};
 
   _.each(_.keys(outputs), (key) => {
-    const value = outputs[key].Value;
+    const outputValue = _.find(stackOutputs, ["OutputKey", key]);
+    const value =
+      outputValue !== undefined && outputValue["OutputValue"] !== undefined
+        ? outputValue["OutputValue"]
+        : outputs[key].Value;
     const envVarKey = _.toUpper(_.snakeCase(key));
     const envVar = { [envVarKey]: value };
     _.assign(envVars, envVar);
